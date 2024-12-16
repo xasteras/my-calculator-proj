17f940bf481b558b03e34a99b7e94f2cd41d6e79
xasteras <xasteras@gmail.com>
2024-12-16 19:43:33 +0200

    project created

---
+++ src/gr/aueb/cf/ch1/AddCalculator.java
@@ -0,0 +1,16 @@
+package gr.aueb.cf.ch1;
+
+public class AddCalculator {
+
+    public static void main(String[] args) {
+        int num1 = 10;
+        int mum2 = 20;
+        int result;
+
+        result = add(num1, mum2);
+
+    }
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
